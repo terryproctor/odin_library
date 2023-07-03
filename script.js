12fd169ad795714d086dc51af81f4813ccd5040b
terryproctor <terryproctor2015@outlook.com>
2023-07-03 22:07:12 +0100

    skeleton for js modal

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementsByClassName('empty')[0].addEventListener('click', () =>{
+    // show the modal
+
+});
+
+book_form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    // create a new book object
+
+    //hide the modal
+
+    //add the book card to the library behind the empty card
+
+});
