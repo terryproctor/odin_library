10c962e898e5622fa4c3bc7d01aca319992da297
terryproctor <terryproctor2015@outlook.com>
2023-07-05 23:06:42 +0100

    MODAL FUNCTIONALITY

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-document.getElementsByClassName('empty')[0].addEventListener('click', () =>{
+document.getElementsByClassName('plus')[0].addEventListener('click', () =>{
     // show the modal
-
+    console.log('clicked');
+    document.getElementsByClassName('modal_background')[0].style.display = 'flex';
 });
 
 book_form.addEventListener('submit', (e) =>{
     e.preventDefault();
+
     // create a new book object
 
     //hide the modal
-
+    document.getElementsByClassName('modal_background')[0].style.display = 'none';
     //add the book card to the library behind the empty card
 
+    //clear the form
 });
